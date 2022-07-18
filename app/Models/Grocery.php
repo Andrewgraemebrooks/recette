56099b6125f01d0278653b1b6cb33a30bdbb4f1a
Andrew Brooks <25846476+Andrewgraemebrooks@users.noreply.github.com>
2022-07-18 22:27:25 +0100

    Created the grocery model

---
+++ app/Models/Grocery.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UsesUuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Grocery extends Model
+{
+    use HasFactory;
+    use UsesUuid;
+}
