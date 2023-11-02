9e5ed2be56d29e1ac4062d861cda59524548ffa1
Andrew Brooks <andrewgraemebrooks@gmail.com>
2023-11-02 09:15:44 +0000

    Encrypt a user's password on registration (#19)

--- tests/Feature/Auth/MobileRegisterationTest.php
+++ tests/Feature/Auth/MobileRegisterationTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature\Auth;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class MobileRegisterationTest extends TestCase
@@ -23,5 +25,24 @@ class MobileRegisterationTest extends TestCase
         $response = $this->postJson('/api/mobile/register', $data);
 
         $response->assertOk();
+    }
+
+    /** @test */
+    public function a_users_password_is_encrypted()
+    {
+        $data = [
+            'name' => 'John Doe',
+            'email' => 'john@doe.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+            'device_name' => 'test device',
+        ];
+
+        $response = $this->postJson('/api/mobile/register', $data);
+
+        $response->assertOk();
+        $user = User::first();
+        $this->assertNotEquals($user->password, 'password');
+        $this->assertTrue(Hash::check('password', $user->password));
     }
 }
